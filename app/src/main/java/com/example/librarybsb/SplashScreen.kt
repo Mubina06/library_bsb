31976e5af14a537ea46d9832555b831dea3cccde
PC672 <mubinashukrullayeva@gmail.com>
2023-03-09 16:01:34 +0500

    lib

--- app/src/main/java/com/example/librarybsb/SplashScreen.kt
+++ app/src/main/java/com/example/librarybsb/SplashScreen.kt
@@ -1,15 +1,87 @@
 package com.example.librarybsb
 
+import android.R
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.librarybsb.databinding.ActivitySplashScreenBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.system.exitProcess
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private var currentLanguage = "en"
+    lateinit var locale: Locale
+    private var currentLang: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val language = ArrayList<String>()
+        language.add("Select Language")
+        language.add("English")
+        language.add("Russian")
+        language.add("Uzbek")
+        val adapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, language)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> {
+                    }
+                    1 -> setLocale("en")
+                    2 -> setLocale("ru")
+                    3 -> setLocale("uz")
+
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
 
 
+        binding.next.setOnClickListener{
+
+        }
+
+    }
+
+        fun setLocale(localeName: String) {
+                locale = Locale(localeName)
+                val res = resources
+                val dm = res.displayMetrics
+                val conf = res.configuration
+                conf.locale = locale
+                res.updateConfiguration(conf, dm)
+                val refresh = Intent(
+                    this,
+                    MainActivity::class.java
+                )
+                refresh.putExtra(currentLang, localeName)
+                startActivity(refresh)
+
+        }
+
+    override fun onBackPressed() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+        exitProcess(0)
     }
 }
